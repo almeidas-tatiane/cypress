4e7fb8ab99aec2e2fb566e22a32649634cbc02e9
almeidas-tatiane <almeidas.tatiane@gmail.com>
2026-03-13 13:31:56 -0300

    Adding context Products

--- cypress/e2e/ecommerce.cy.js
+++ cypress/e2e/ecommerce.cy.js
@@ -7,23 +7,102 @@ describe('ecommerce', () => {
       cy.ValidLogin()
     })
 
-
-
-      it('Logout', () => {
-        cy.ValidLogin()
-        cy.get('.user-name')
-          .should('be.visible')
-          .should('have.text', 'test@qabrains.com')
-        cy.get('span.caret')
-          .click()
-        cy.get('.text-red-500')
-          .click()
-        cy.contains('button', 'Logout')
-          .click()
-        cy.contains('button', 'Login')
-          .should('be.visible')
-          .should('have.text', 'Login')
-      })
+    it('Logout', () => {
+      cy.ValidLogin()
+      cy.get('.user-name')
+        .should('be.visible')
+        .should('have.text', 'test@qabrains.com')
+      cy.get('span.caret')
+        .click()
+      cy.get('.text-red-500')
+        .click()
+      cy.contains('button', 'Logout')
+        .click()
+      cy.contains('button', 'Login')
+        .should('be.visible')
+        .should('have.text', 'Login')
     })
   })
 
+  context('Products', () => {
+    it('ValidateProductsPage', () => {
+      cy.ValidLogin()
+      cy.ValidateProductsPage()
+
+    })
+
+
+    it.only('OrderByProducts', () => {
+      cy.ValidLogin()
+      cy.ValidateProductsPage()
+      cy.contains('p', 'Order by')
+        .should('be.visible')
+        .should('have.text', 'Order by')
+      cy.contains('button', 'Select...')
+        .should('be.visible')
+        .should('have.text', 'Select...')
+        .click()
+
+
+      cy.contains('div', 'A to Z (Ascending)')
+        .should('be.visible')
+        .should('have.text', 'A to Z (Ascending)')
+        .click()
+      cy.contains('a.text-lg', 'Sample Shirt Name')
+        .should('be.visible')
+        .should('have.text', 'Sample Shirt Name')
+
+
+      cy.contains('div', 'A to Z (Ascending)')
+        .click()
+      cy.contains('div', 'Z to A (Descending)')
+        .should('be.visible')
+        .should('have.text', 'Z to A (Descending)')
+        .click()
+      cy.contains('a.text-lg', 'Sample T-Shirt Name')
+        .should('be.visible')
+        .should('have.text', 'Sample T-Shirt Name')
+
+
+      cy.contains('div', 'Z to A (Descending)')
+        .click()
+      cy.contains('div', 'Low to High (Price)')
+        .should('be.visible')
+        .should('have.text', 'Low to High (Price)')
+        .click()
+      cy.contains('a.text-lg', 'Sample T-Shirt Name')
+        .should('be.visible')
+        .should('have.text', 'Sample T-Shirt Name')
+      cy.get('.products > :nth-child(1) > .flex > .text-lg')
+        .should('have.text', '$45.00')
+
+
+      cy.contains('div', 'Low to High (Price)')
+        .click()
+      cy.contains('div', 'High to Low (Price)')
+        .should('be.visible')
+        .should('have.text', 'High to Low (Price)')
+        .click()
+      cy.contains('a.text-lg', 'Sample Sunglass Name')
+        .should('be.visible')
+        .should('have.text', 'Sample Sunglass Name')
+      cy.get('.products > :nth-child(1) > .flex > .text-lg')
+        .should('have.text', '$256.45')
+
+
+      cy.contains('div', 'High to Low (Price)')
+        .click()
+      cy.contains('div', 'A to Z (Ascending)')
+        .should('be.visible')
+        .should('have.text', 'A to Z (Ascending)')
+        .click()
+
+
+
+
+
+    })
+  })
+
+
+})
