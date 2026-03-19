469173f34a9fc23f9ea60da5db38c41d3ca7d384
almeidas-tatiane <almeidas.tatiane@gmail.com>
2026-03-19 16:56:51 -0300

    Adding Allure Report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('@shelex/cypress-allure-plugin/writer')(on, config)
+      return config
+
     },
   },
 });
