0cf83d95576cf22ced1de989e904cf0a1bcde1c2
Tatiane da Silva Almeida <68197687+almeidas-tatiane@users.noreply.github.com>
2026-03-20 17:30:17 -0300

    Add projectId to Cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'j6r5fz',
   e2e: {
     setupNodeEvents(on, config) {
       require('@shelex/cypress-allure-plugin/writer')(on, config)
